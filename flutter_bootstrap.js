0391305c2856ad7105b3c446aeae7630b657022a
“lovepreetSinghElectricpe” <lovepreet@electricpe.com>
2024-11-19 12:49:39 +0530

    build with href change

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2650609447"
+    serviceWorkerVersion: "1105532660"
   }
 });
